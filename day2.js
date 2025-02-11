28528ae937ff23f525ba87990b5d16ca1d4af580
Junaid Shah <junaidxdev@gmail.com>
2025-02-11 18:47:50 +0500

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,54 @@
+// ----------------- Operators -------------------
+
+// In JavaScript, operators are symbols that perform operations on variables and values.
+
+// 1. Arithmetic Operators
+
+// +	Addition    	a + b	15
+// -	Subtraction 	a - b	5
+// *	Multiplication  a * b	50
+// /	Division    	a / b	2
+// %	Modulus  	    a % b	0
+// **	Exponentiation  a **    2	100
+// ++	Increment	    a++	    11 (after execution)
+// --	Decrement	    b--	    4 (after execution)
+
+
+// 2. Assignment Operators
+
+// =	Assign  	                x = 10	    10
+// +=	Add and Assign  	        x += 5	    15
+// -=	Subtract and Assign	        x -= 3	    7
+// *=	Multiply and Assign	        x *= 2	    20
+// /=	Divide and Assign	        x /= 5	    2
+// %=	Modulus and Assign	        x %= 3	    1
+// **=	Exponentiation and Assign	x **= 2	    100
+
+
+// 3. Comparison Operators
+
+// ==	    Equal to    	        a == 10	    true
+// ===	    Strict Equal	        a === "10"	false
+// !=	    Not Equal	            a != b	    true
+// !==	    Strict Not Equal	    a !== "10"	true
+// >	    Greater than	        a > b	    true
+// <	    Less than	            a < b	    false
+// >=	    Greater than or equal	a >= 10	    true
+// <=	    Less than or equal	    b <= 5	    true
+
+
+// 4. Logical Operators
+
+// &&  	AND 	x && y  	false
+// ||   	||                  OR
+// !   	NOT 	!x	        false
+
+// 6. Ternary Operator
+
+let age = 18;
+let status = (age >= 18) ? "Adult" : "Minor";
+console.log(status); // Output: "Adult"
+
+
+
+// ----------------- End ------------------
