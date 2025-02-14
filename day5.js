bb12ac56d855902e47042d4ced048dced2fc9f26
Junaid Shah <junaidxdev@gmail.com>
2025-02-14 18:25:20 +0500

    Add files via upload

---
+++ day5.js
@@ -0,0 +1,47 @@
+// --------------------- Loops ------------------------
+
+
+// 1: Print Numbers from 1 to 10
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+       
+}
+
+
+// 2: Print Even Numbers from 2 to 20
+
+for (let i = 2; i <= 20; i++) {
+    if (i%2==0) {
+        console.log(i);
+        
+    }
+}
+
+
+// 3: Sum of First 10 Natural Numbers
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+    sum = sum + i;
+    
+}
+console.log(sum);
+
+
+// 4: Reverse Countdown from 10 to 1
+
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+    
+}
+
+// 5: Multiplication Table of 5
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i*5);
+    
+}
+
+// ------------------------ End --------------------------
